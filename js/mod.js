cfb37d22ae9a2f9f9ca29a4ff9ed2638c2f5e562
4xSp33d <istillwantmyfamilyback@gmail.com>
2023-05-17 17:59:54 -0400

    Update mod.js
    rename

--- js/mod.js
+++ js/mod.js
@@ -1,6 +1,6 @@
 let modInfo = {
-	name: "The ??? Tree",
-	id: "mymod",
+	name: "The big giant coc Tree",
+	id: "myhugecoc",
 	author: "nobody",
 	pointsName: "points",
 	modFiles: ["layers.js", "tree.js"],
